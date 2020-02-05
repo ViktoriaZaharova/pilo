706e638094100726e6b4390b7421c44d90ccce38
viktoriazaharova <zaharova181114@gmail.com>
2020-02-05 19:30:53 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,15 @@
+$('.btn-size').click(function (e) {
+    e.preventDefault();
+    $(this).siblings('.box-hidden').fadeToggle();
+});
+
+$('.go_to').click(function () {
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length != 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    return false;
+});
+
