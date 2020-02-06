c17e2b102cccb5d892ba8c125cb52cfe3807aeb5
viktoriazaharova <zaharova181114@gmail.com>
2020-02-06 18:01:41 +0200

    css + animate

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,7 @@
 $('.btn-size').click(function (e) {
     e.preventDefault();
     $(this).siblings('.box-hidden').fadeToggle();
+    $(this).parents('.product-box__wrapper').toggleClass('click');
 });
 
 $('.go_to').click(function () {
@@ -12,4 +13,31 @@ $('.go_to').click(function () {
     }
     return false;
 });
+
+$('.checkbox-phone').click(function () {
+   $('.form-box__title-phone').fadeIn();
+   $('.form-box__title-whatsapp').hide();
+});
+
+$('.checkbox-whatsapp').click(function () {
+    $('.form-box__title-whatsapp').fadeIn();
+    $('.form-box__title-phone').hide();
+});
+
+var wow = new WOW(
+    {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       true,       // trigger animations on mobile devices (default is true)
+        live:         true,       // act on asynchronously loaded content (default is true)
+        callback:     function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null,    // optional scroll container selector, otherwise use window,
+        resetAnimation: true,     // reset animation on end (default is true)
+    }
+);
+wow.init();
 
